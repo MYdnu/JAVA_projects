00823743f964847da2525cd6f5ca5f98eca3f684
ydnu <fanosquad405@gmail.com>
2022-07-21 17:52:31 +0300

    updated file

--- factorial.java
+++ factorial.java
@@ -10,7 +10,8 @@ public class factorial {
          int i,fact=1;  
          int number=4;//It is the number to calculate factorial    
          fact = factorial(number);   
-         System.out.println("Factorial of "+number+" is: "+fact);    
+         System.out.println("Factorial of "+number+" is: "+fact);  
+         System.out.println("Factorial of "+number+" is: "+fact);  
         }  
        
     
